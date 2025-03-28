b3f59c138a0e227a1d058017a50f7fbc3fc2906b
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-03-28 13:22:22 +0100

    funktionalitet på cookie popup

--- src/app/components/CookiePopup.jsx
+++ src/app/components/CookiePopup.jsx
@@ -1,17 +1,25 @@
 "use client";
+import { use, useState } from "react";
 import { BiSolidCookie } from "react-icons/bi";
 import { RxCross2 } from "react-icons/rx";
 
 const CookiePopup = () => {
+  const [cookiesAccepted, setCookiesAccepted] = useState(false);
   return (
-    <article className="cookiePopup">
-      <div className="icons">
-        <BiSolidCookie className="cookie" />
-        <RxCross2 className="cross" />
-      </div>
-      <p className="paragraph">We use cookies to improve your user experience</p>
-      <button className="button">I like Cookies</button>
-    </article>
+    // && hvis er true //
+    !cookiesAccepted && (
+      /* Hvis det er sandt at værdien er cookie accepted er true, så hvis følgende. Ellers lad være */
+      <article className="cookiePopup">
+        <div className="icons">
+          <BiSolidCookie className="cookie" />
+          <RxCross2 className="cross" />
+        </div>
+        <p className="paragraph">We use cookies to improve your user experience</p>
+        <button onClick={() => setCookiesAccepted(!cookiesAccepted)} /* Sætter værdien til det modsatte */ className="button">
+          I like Cookies
+        </button>
+      </article>
+    )
   );
 };
 
